6933cf218158ec9c3be68bb5e0b5a3e912c5bb0e
yassinebk <yassinebk23@gmail.com>
2022-03-01 02:00:10 +0100

    Project Finished

--- src/AppFrame.java
+++ src/AppFrame.java
@@ -8,7 +8,9 @@ import java.util.ArrayList;
 
 public class AppFrame extends Frame implements ActionListener, WindowListener {
 
-  final String[] operators = {"/", "+", "-", "%","*","=",};
+  final String[] operators = {
+          "/", "+", "-", "%", "*", "=",
+  };
   ArrayList<NumberButton> numberButtons;
   ArrayList<OperatorButton> operatorButtons;
   AppController appController;
@@ -16,82 +18,85 @@ public class AppFrame extends Frame implements ActionListener, WindowListener {
   ArrayList<Panel> calculatorPanels;
   Panel mainPanel;
 
-
   AppFrame() {
     addWindowListener(this);
     calculatorPanels = new ArrayList<>();
-    appController=new AppController();
-    numberButtons =new ArrayList<>();
-    mainPanel=new Panel();
-    mainPanel.setLayout(new GridLayout(7,1));
-
-
+    appController = new AppController();
+    numberButtons = new ArrayList<>();
+    mainPanel = new Panel();
+    mainPanel.setLayout(new GridLayout(7, 1));
 
     for (int i = 0; i < 10; i++) {
-      if(i%3==0){
+      if (i % 3 == 0) {
         Panel panel = new Panel();
         panel.setLayout(new FlowLayout());
         int i1 = i / 3;
-        System.out.println("i1"+i1);
+        System.out.println("i1" + i1);
         panel.setVisible(true);
         calculatorPanels.add(panel);
       }
-      NumberButton btn = new NumberButton(Integer.toString(i),appController);
+      NumberButton btn = new NumberButton(Integer.toString(i), appController);
 
       numberButtons.add(btn);
-      calculatorPanels.get(calculatorPanels.size()-1).add(btn);
+      calculatorPanels.get(calculatorPanels.size() - 1).add(btn);
     }
 
     calculatorPanels.add(new Panel());
     for (String operator : operators) {
-      operatorButtons= new ArrayList<>();
-      OperatorButton btn = new OperatorButton(operator,appController);
+      operatorButtons = new ArrayList<>();
+      OperatorButton btn = new OperatorButton(operator, appController);
       operatorButtons.add(btn);
-      calculatorPanels.get(calculatorPanels.size()-1).add(btn);
+      calculatorPanels.get(calculatorPanels.size() - 1).add(btn);
     }
 
-    for (Panel panel: calculatorPanels){
+    for (Panel panel : calculatorPanels) {
       mainPanel.add(panel);
     }
 
-    Font font = new Font("Arial Black",Font.PLAIN,30);
-    setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+    Font font = new Font("Arial Black", Font.PLAIN, 30);
+    setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
     setFont(font);
     this.add(mainPanel);
     this.add(appController.labelComp);
     this.setLocationRelativeTo(null);
-    this.setSize(900,750);
+    this.setSize(900, 750);
     this.setResizable(true);
     this.setVisible(true);
   }
 
-    private void setWindowKeyListener(){}
-
-    @Override
-    public void actionPerformed(ActionEvent actionEvent) {}
-
-    @Override
-    public void windowOpened(WindowEvent windowEvent) {}
-
-    @Override
-    public void windowClosing(WindowEvent windowEvent) {
-    dispose();
-    }
-
-    @Override
-    public void windowClosed(WindowEvent windowEvent) {
-
-    }
-
-    @Override
-    public void windowIconified(WindowEvent windowEvent) {}
-
-    @Override
-    public void windowDeiconified(WindowEvent windowEvent) {}
-
-    @Override
-    public void windowActivated(WindowEvent windowEvent) {}
-
-    @Override
-    public void windowDeactivated(WindowEvent windowEvent) {}
+  private void setWindowKeyListener() {
   }
+
+  @Override
+  public void actionPerformed(ActionEvent actionEvent) {
+  }
+
+  @Override
+  public void windowOpened(WindowEvent windowEvent) {
+  }
+
+  @Override
+  public void windowClosing(WindowEvent windowEvent) {
+    dispose();
+  }
+
+  @Override
+  public void windowClosed(WindowEvent windowEvent) {
+  }
+
+  @Override
+  public void windowIconified(WindowEvent windowEvent) {
+  }
+
+  @Override
+  public void windowDeiconified(WindowEvent windowEvent) {
+  }
+
+  @Override
+  public void windowActivated(WindowEvent windowEvent) {
+  }
+
+  @Override
+  public void windowDeactivated(WindowEvent windowEvent) {
+  }
+}
